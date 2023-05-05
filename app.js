2ad086fdce850a951177302e161ce80d67271251
Atanu Paul <atanupaul2411@gmail.com>
2023-05-05 23:49:14 +0530

    news card fixed

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('hello');
+const news = document.querySelector('.news');
+const btn = document.querySelector('#btn');
+
+news.addEventListener('mouseover', () => btn.style.display = 'block');
+news.addEventListener('mouseout', () => btn.style.display = 'none');
