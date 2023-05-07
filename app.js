9b0bb279434215ecbbbfe764a480aa0db9996209
Atanu Paul <atanupaul2411@gmail.com>
2023-05-07 11:22:16 +0530

    all functionalities added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const apiKey = 'O0RhBQBVGLfvp60MJIn8o5KaoucVaZOi';
-let rows = document.querySelectorAll('.row');
-let viewBtns = document.querySelectorAll('.viewBtn');
+const rows = document.querySelectorAll('.row');
+const viewBtns = document.querySelectorAll('.viewBtn');
+const search = document.querySelector('#searchNews');
+const searchBtn = document.querySelector('#searchBtn');
 
 rowItems = {
     rowName: ["sports", "automobiles", "politics", "movies", "world"]
@@ -35,30 +37,46 @@ function defaultNews(topics, rowNum) {
         })
 }
 
-// function getNews() {
-//     $('.posts').text("");
-//     let keyword = $('#keyword').val();
-//     const search_url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=${apiKey}`;
-//     console.log(search_url);
-//     if (keyword == '') {
-//         alert("Please input something!");
-//         defaultNews();
-//     } else {
-//         fetch(search_url)
-//             .then(response => {
-//                 return response.json();
-//             })
-//             .then(data => {
-//                 for (i = 0; i < data.response.docs.length; i++) {
-//                     let html = `<div class="news">
-//                     <div class="img">
-//                         <img src="${data.results[i].multimedia[0].url}"/>
-//                     </div>
-//                     <h3>${data.results[i].title}</h3>
-//                     <a href="${data.results[i].url}" target="_blank" ><button id="btn">read more</button></a>
-//                 </div>`;
-//                     // $('.posts').append(html);
-//                 }
-//             })
-//     }
-// }
+viewBtns.forEach((element, i) => {
+    let keyword = rowItems.rowName[i]
+    element.addEventListener('click', () => getNews(keyword));
+});
+
+searchBtn.addEventListener('click', () => {
+    let searchKey = search.value.trim();
+    if (searchKey === ''){
+        alert('Search for nothing');
+    }
+    else {
+        getNews(searchKey);
+    }
+});
+
+function getNews(searchKey) {
+    $('.news-wrapper').text("");
+    const url = `https://api.nytimes.com/svc/topstories/v2/${searchKey}.json?api-key=${apiKey}`;
+    let allNews = '';
+    fetch(url)  // todo: fixed the 404 not found error
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            for (i = 1; i < data.results.length; i++) {
+                let news = `
+                    <div class="news">
+                    <div class="img">
+                        <img src="${data.results[i].multimedia[0].url}"/>
+                    </div>
+                    <h3>${data.results[i].title}</h3>
+                    <a href="${data.results[i].url}" target="_blank" ><button id="btn">read more</button></a>
+                    </div>`;                
+                allNews += news;
+                console.log(data.results[i].multimedia[0].url); // todo: caught (in promise) TypeError: Cannot read properties of null (reading '0')
+            }
+            $('.news-wrapper').append(`<div class="row"> ${allNews} </div>`);
+        })
+        .catch(error => {
+            alert('Not Found')
+            console.log(error);
+        });
+}
