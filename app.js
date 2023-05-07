6a771f7d4daa92c4c25684a58e328fce76d846ec
Atanu Paul <atanupaul2411@gmail.com>
2023-05-07 09:18:12 +0530

    default view created

--- app.js
+++ app.js
@@ -1,7 +1,64 @@
-const news = document.querySelectorAll('.news');
-const btn = document.querySelectorAll('#btn');
+const apiKey = 'O0RhBQBVGLfvp60MJIn8o5KaoucVaZOi';
+let rows = document.querySelectorAll('.row');
+let viewBtns = document.querySelectorAll('.viewBtn');
 
-news.forEach((e, b) => {
-    e.addEventListener('mouseover', () => (btn[b].style.display = 'block'));
-    e.addEventListener('mouseout', () => (btn[b].style.display = 'none'));
+rowItems = {
+    rowName: ["sports", "automobiles", "politics", "movies", "world"]
+}
+
+rowItems.rowName.forEach((element, i) => {
+    defaultNews(element, i)
 });
+
+function defaultNews(topics, rowNum) {
+    const url = `https://api.nytimes.com/svc/topstories/v2/${topics}.json?api-key=${apiKey}`;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            for (i = 1; i <= 6; i++) {
+
+                let html = `<div class="news">
+                        <div class="img">
+                            <img src="${data.results[i].multimedia[0].url}"/>
+                        </div>
+                        <h3>${data.results[i].title}</h3>
+                        <a href="${data.results[i].url}" target="_blank" ><button id="btn">read more</button></a>
+                    </div>`;
+                $(rows[rowNum]).append(html);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+// function getNews() {
+//     $('.posts').text("");
+//     let keyword = $('#keyword').val();
+//     const search_url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=${apiKey}`;
+//     console.log(search_url);
+//     if (keyword == '') {
+//         alert("Please input something!");
+//         defaultNews();
+//     } else {
+//         fetch(search_url)
+//             .then(response => {
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 for (i = 0; i < data.response.docs.length; i++) {
+//                     let html = `<div class="news">
+//                     <div class="img">
+//                         <img src="${data.results[i].multimedia[0].url}"/>
+//                     </div>
+//                     <h3>${data.results[i].title}</h3>
+//                     <a href="${data.results[i].url}" target="_blank" ><button id="btn">read more</button></a>
+//                 </div>`;
+//                     // $('.posts').append(html);
+//                 }
+//             })
+//     }
+// }
